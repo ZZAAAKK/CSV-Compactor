64fab8bded77a86ba55502b81f19f6b84e1f0ba9
ZZAAAKK <48885763+ZZAAAKK@users.noreply.github.com>
2022-03-11 21:08:47 +0000

    Vastly improved basically everything.

--- CSV Compactor/CSVCompactor.fsx
+++ CSV Compactor/CSVCompactor.fsx
@@ -1,44 +1,19 @@
 ﻿open System.IO
-open System.Data
-open System.Linq
 
 let DiscoverFiles directory = [ for file in Directory.GetFiles(directory) do if Path.GetExtension(file) = ".csv" then yield file ]
 
-let ReadFilesIntoMemory files =
-    [ for file in files do
-        let dt = new DataTable(file)
-        printfn $"Loading {dt.TableName}"
-        use sr = new StreamReader(file)
-        dt.Columns.AddRange([| for header in sr.ReadLine().Split(',') do yield new DataColumn(header) |])
-        while not sr.EndOfStream do
-            let sb = new System.Text.StringBuilder()
-            for s in sr.ReadLine().Split(',') do sb.Append($"{s},") |> ignore
-            dt.Rows.Add(sb.ToString()) |> ignore
-        yield dt ]
-
-let CompareHeaders (dataSet : DataTable list) =
-    let rec Compare (data : DataTable list) = 
-        match data with
-        | [_] -> true
-        | h::t -> Enumerable.SequenceEqual([ for c in h.Columns do yield c.ColumnName ], [for c in t.Head.Columns do yield c.ColumnName ]) || Compare t
-        | _ -> false
-    Compare dataSet, dataSet
+let ReadFilesIntoMemory (files : seq<string>) =
+    [ for file in files ->
+        [ use sr = new StreamReader(file)
+        yield! [ while not sr.EndOfStream do yield sr.ReadLine() ]]]
     
-let CreateCompactedCSV matchingHeaders (dataSet : DataTable list) =
-    if matchingHeaders then
+let TryCreateCompactedCSV (data : string list list) =
+    data |> Seq.map (fun x -> x.Head) |> Seq.distinct |> Seq.length |> function
+    | 1 -> 
         use sw = fsi.CommandLineArgs.[2] |> File.CreateText
-        for column in dataSet.[0].Columns do sw.Write($"{column.ColumnName},")
-        sw.WriteLine()
-        for table in dataSet do
-            for row in table.Rows do
-                for item in row.ItemArray do 
-                    sw.Write($"{item}")
-                sw.WriteLine()
+        sw.WriteLine(data.Head.Head)
+        [[for rows in data do yield! rows.Tail]] |> Seq.concat |> Seq.iter (fun row -> sw.WriteLine(row))
         printfn "New file created successfully!"
-    else printfn "Incompatible headers found - aborted process!"
+    | _ -> printfn "Incompatible headers found - aborted process!"
 
-fsi.CommandLineArgs.[1]
-|> DiscoverFiles
-|> ReadFilesIntoMemory
-|> CompareHeaders
-||> CreateCompactedCSV
+fsi.CommandLineArgs.[1] |> DiscoverFiles |> ReadFilesIntoMemory |> TryCreateCompactedCSV
