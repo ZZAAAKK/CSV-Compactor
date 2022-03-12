ddb48cc1bbd8fd0f298b3a6df489609894f3c1d8
ZZAAAKK <48885763+ZZAAAKK@users.noreply.github.com>
2022-03-12 14:20:22 +0000

    Added logging and safeguarding against errors.

--- CSV Compactor/CSVCompactor.fsx
+++ CSV Compactor/CSVCompactor.fsx
@@ -1,19 +1,22 @@
 ﻿open System.IO
 
-let DiscoverFiles directory = [ for file in Directory.GetFiles(directory) do if Path.GetExtension(file) = ".csv" then yield file ]
-
-let ReadFilesIntoMemory (files : seq<string>) =
-    [ for file in files ->
-        [ use sr = new StreamReader(file)
-        yield! [ while not sr.EndOfStream do yield sr.ReadLine() ]]]
+let ReadFilesIntoMemory directory =
+    if Directory.Exists directory then 
+        [ for file in [ for file in Directory.GetFiles(directory) do if Path.GetExtension(file) = ".csv" then printfn "Found file %s" file; yield file ] ->
+            [ use sr = new StreamReader(file)
+            yield! [ while not sr.EndOfStream do yield sr.ReadLine() ]]]
+    else printfn "The specified input directory could not be found!"; []
     
 let TryCreateCompactedCSV (data : string list list) =
-    data |> Seq.map (fun x -> x.Head) |> Seq.distinct |> Seq.length |> function
-    | 1 -> 
-        use sw = fsi.CommandLineArgs.[2] |> File.CreateText
-        sw.WriteLine(data.Head.Head)
-        [[for rows in data do yield! rows.Tail]] |> Seq.concat |> Seq.iter (fun row -> sw.WriteLine(row))
-        printfn "New file created successfully!"
-    | _ -> printfn "Incompatible headers found - aborted process!"
+    data |> Seq.length |> function
+    | 0 -> printfn "No files found!"
+    | _ as n -> printfn "Comparing headers for %i files." n
+                data |> Seq.map (fun x -> x.Head) |> Seq.distinct |> Seq.length |> function
+                | 1 -> 
+                    use sw = fsi.CommandLineArgs.[2] |> File.CreateText
+                    sw.WriteLine(data.Head.Head)
+                    [[for rows in data do yield! rows.Tail]] |> Seq.concat |> Seq.iter (fun row -> sw.WriteLine(row))
+                    printfn "New file created successfully!"
+                | _ -> printfn "Incompatible headers found - aborted process!"
 
-fsi.CommandLineArgs.[1] |> DiscoverFiles |> ReadFilesIntoMemory |> TryCreateCompactedCSV
+fsi.CommandLineArgs.[1] |> ReadFilesIntoMemory |> TryCreateCompactedCSV
